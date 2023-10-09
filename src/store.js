d1fb8909e574d84504b2c92e88e3fd4b11a9097c
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-10-09 18:39:04 +0330

    Start :: working on redux toolkit

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 import thunk from "redux-thunk";
